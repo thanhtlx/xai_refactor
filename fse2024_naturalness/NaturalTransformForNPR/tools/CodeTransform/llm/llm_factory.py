dcefd65139e8152dc26dccd4d29571d2f5fad25b
Vũ Trọng Thanh <52902396+thanhtlx@users.noreply.github.com>
2024-03-09 21:02:08 +0700

    Delete fse2024_naturalness/NaturalTransformForNPR directory

--- fse2024_naturalness/NaturalTransformForNPR/tools/CodeTransform/llm/llm_factory.py
+++
@@ -1,9 +0,0 @@
-from transformers import RobertaConfig, RobertaTokenizer, RobertaForMaskedLM, pipeline
-from llm.llm import CodeBERTProbing
-
-class LLMFactory(object):
-    def create(llm_name, top_k):
-        if llm_name == 'CodeBERT':
-            return CodeBERTProbing(llm_name, top_k)
-        else:
-            raise ValueError('Invalid LLM name. Currently we only support [CodeBERT]')
